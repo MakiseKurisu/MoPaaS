480735b52ba33f9945c8c53d7f1da41debc61ffb
MakiseKurisu <saberconer@gmail.com>
2014-06-09 11:59:19 +0800

    .

--- mysqltest.php
+++ mysqltest.php
@@ -1,14 +1,21 @@
-﻿<?php
-
-require_once('./config.php');
+<?php
  
-/*调用mysql_connect()连接服务器*/
-$link = @mysql_connect($DB_HOST,$DB_USER,$DB_PASSWORD,true);
+/*替换为你自己的数据库名*/
+$dbname = 'd8cee8633cd0949d7986a36d87b211565';
+/*填入数据库连接信息*/
+$host = '10.4.14.186';
+$port = 3306;
+$user = 'u5xsu1W8slnuT';//用户名
+$pwd = 'ph1PEuGxGnrVR';//密码
+ /*以上信息都可以在数据库管里页面查找到*/
+ 
+/*接着调用mysql_connect()连接服务器*/
+$link = @mysql_connect("{$host}:{$port}",$user,$pwd,true);
 if(!$link) {
     die("Connect Server Failed: " . mysql_error());
 }
 /*连接成功后立即调用mysql_select_db()选中需要连接的数据库*/
-if(!mysql_select_db($DB_NAME,$link)) {
+if(!mysql_select_db($dbname,$link)) {
     die("Select Database Failed: " . mysql_error($link));
 }
  

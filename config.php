6e392fb3c26b665082b0ebaa5535e221422cb4af
MakiseKurisu <saberconer@gmail.com>
2014-06-09 18:11:49 +0800

    Small tweak

--- config.php
+++ config.php
@@ -1,13 +1,15 @@
 <?php
-	/*
-	$DB_HOST = 'localhost:3306';
+	///*
+	$DB_HOST = 'localhost';
+	$DB_PORT = 3306;
 	$DB_USER = 'root';
 	$DB_PASSWORD = 'password';
 	$DB_NAME = 'SampleDB';
 	//*/
 
-	///*
-	$DB_HOST = '10.4.14.186:3306';
+	/*
+	$DB_HOST = '10.4.14.186';
+	$DB_PORT = 3306;
 	$DB_USER = 'u5xsu1W8slnuT';
 	$DB_PASSWORD = 'ph1PEuGxGnrVR';
 	$DB_NAME = 'd8cee8633cd0949d7986a36d87b211565';

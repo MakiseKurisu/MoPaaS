17ec6b34cd78b160297cd97a455f2ddb9d69bde6
MakiseKurisu <saberconer@gmail.com>
2014-06-12 09:48:25 +0800

    Let's see if this will fix
    Unknown MySQL server host '10.4.14.186:3306'

--- config.php
+++ config.php
@@ -11,7 +11,7 @@
 	{
 		$SERVICE_ID = '10770';
 		$DB_HOST = getenv("MOPAAS_MYSQL{$SERVICE_ID}_HOST");
-		$DB_PORT = getenv("MOPAAS_MYSQL{$SERVICE_ID}_PORT");
+		$DB_PORT = (int) getenv("MOPAAS_MYSQL{$SERVICE_ID}_PORT");
 		$DB_USER = getenv("MOPAAS_MYSQL{$SERVICE_ID}_USER");
 		$DB_PASSWORD = getenv("MOPAAS_MYSQL{$SERVICE_ID}_PASSWORD");
 		$DB_NAME = getenv("MOPAAS_MYSQL{$SERVICE_ID}_NAME");
@@ -26,9 +26,11 @@
 	{
 		// Connect to MySQL server
 		$database = mysqli_connect(
-			"{$GLOBALS['DB_HOST']}:{$GLOBALS['DB_PORT']}",
+			"{$GLOBALS['DB_HOST']}",
 			$GLOBALS['DB_USER'],
-			$GLOBALS['DB_PASSWORD']);
+			$GLOBALS['DB_PASSWORD'],
+			null,
+			$GLOBALS['DB_PORT']);
 		if (mysqli_connect_errno())
 		{
 			$return['Code'] = 0;
